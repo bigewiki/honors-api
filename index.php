225fc484d99e4639149390ea196475b9da6c9d7f
Edward M <edwardtmuniz@gmail.com>
2019-10-09 20:29:56 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+// change this appropriately
+include('/home/stu/superuser/db.inc.php');
+
+$uri = explode("/",trim($_SERVER['REQUEST_URI']));
+
+// print_r ($uri);
+
+$returnData = array();
+
+echo "<br/>";
+echo "hello";
+
+
+$sql = "select * from orders";
+$result = mysqli_query($conn, $sql);
+print_r(mysqli_fetch_assoc($result));
+
+// close connection
+$conn->close();
+?>
