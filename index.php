1f1307966bd8989d041931034bb047960e1c7ddf
Edward M <edwardtmuniz@gmail.com>
2019-10-12 20:11:32 -0700

    working on making this more oop

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
 <?php
-
-// change this appropriately
-
+declare(strict_types=1);
 require('../../db.inc.php');
 
-$uri = explode("/",trim($_SERVER['REQUEST_URI']));
+class apiInit extends mysqli{
+    function getUri(){
+        return explode("/",trim($_SERVER['REQUEST_URI']));
+    }
+}
 
-echo $_SERVER['REMOTE_ADDR'];
-
-// print_r ($uri);
-
-$returnData = array();
-
+$apiInit = new apiInit($servername, $username, $password, $dbname);
+if ($apiInit->connect_error) {
+    die("Connection failed: " . $apiInit->connect_error);
+}
 
 $sql = "select * from users";
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($apiInit, $sql);
 print_r(mysqli_fetch_assoc($result));
+$apiInit->close();
 
-// close connection
-$conn->close();
 
-echo "just checking";
 ?>
