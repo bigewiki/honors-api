4c9f743a5b336fc4c6a3424e5d727e1e8251db9d
Edward M <edwardtmuniz@gmail.com>
2019-10-12 18:11:22 -0700

    working local

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
 // change this appropriately
-include('/home/stu/superuser/db.inc.php');
+
+require('../../db.inc.php');
 
 $uri = explode("/",trim($_SERVER['REQUEST_URI']));
+
+echo $_SERVER['REMOTE_ADDR'];
 
 // print_r ($uri);
 
 $returnData = array();
 
-echo "<br/>";
-echo "hello";
 
-
-$sql = "select * from orders";
+$sql = "select * from users";
 $result = mysqli_query($conn, $sql);
 print_r(mysqli_fetch_assoc($result));
 
