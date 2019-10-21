80ffa48e4b85612686f668e8fcbd04bbf79b4b09
Edward M <edwardtmuniz@gmail.com>
2019-10-20 19:14:25 -0700

    working story route for specific stories

--- inc/stories.inc.php
+++ inc/stories.inc.php
@@ -10,8 +10,22 @@ class Stories{
                 break;
             default:
                 if(is_numeric($route)){
-                    $tasks = $Api->getSqlArray("call displayStoryTasks($route)");
-                    $output = array("tasks"=>$tasks);
+                    $query  = "CALL displayStory($route);";
+                    $query .= "CALL displayStoryTasks($route)";
+                    //get the query result in an array
+                    if ($Api->multi_query($query)) {
+                        do {
+                            if ($result = $Api->store_result()) {
+                                while ($row = $result->fetch_assoc()) {
+                                    $results[] = $row;
+                                }
+                                $result->free();
+                            }
+                        } while ($Api->next_result());
+                    }
+                    $output = $results[0];
+                    array_shift($results);
+                    $output['tasks']=$results;
                     $Api->arrayToJson($output);
                 } else {
                     $Api->notFound();
