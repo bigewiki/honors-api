9f59d1aed2fd0df8e8a09e76e79411100ccc2db2
Edward M <edwardtmuniz@gmail.com>
2019-12-01 21:45:52 -0700

    bad path in config

--- config.inc.php
+++ config.inc.php
@@ -5,7 +5,7 @@ if( $hostname == 'localhost'){
     require_once('/var/www/db.inc.php');
     $docRoot = '/edward/honors-api/';
 } else {
-    require_once('/home/students/superuser/db.inc.php');
+    require_once('/home/stu/superuser/db.inc.php');
     $docRoot = '/superuser/honors-api/';
 }
 
